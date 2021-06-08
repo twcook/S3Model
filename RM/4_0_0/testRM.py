fd1f5d10ba29803924a1c07f4640f3c8c5358925
Tim Cook <timothywayne.cook@gmail.com>
2021-06-08 09:58:39 -0500

    Change all classes to be subclass of RM

--- RM/4_0_0/testRM.py
+++ RM/4_0_0/testRM.py
@@ -1,4 +1,7 @@
+import sys 
+
 from pyshacl import validate
+
 r = validate('s3mcjff70c8904bblzl3ujhclwu0.ttl',
       shacl_graph='s3model_4_0_0_Shapes.ttl',
       ont_graph='s3model_4_0_0.ttl',
@@ -13,8 +16,15 @@ conforms, results_graph, results_text = r
 print('============')
 print('============')
 print('============')
-# print('Results Graph: ', '/n', results_graph)
+stdoutOrigin=sys.stdout 
+sys.stdout = open("output_graph.ttl", "w")
+print('Results Graph: ', '/n', results_graph)
 print('============')
+
+sys.stdout = open("results.txt", "w")
+
 print('Results Text: ', results_text)
+sys.stdout.close()
+sys.stdout=stdoutOrigin
 print('============')
 print('Conforms: ', conforms)
